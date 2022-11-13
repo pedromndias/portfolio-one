59b763214e60dd1801dc2a288dde8096af35fb12
Pedro Dias <pedromndias@gmail.com>
2022-11-13 11:30:15 +0100

    updated modal close button

--- blog.js
+++ blog.js
@@ -1,7 +1,12 @@
 // In this file we only need to have JS for the modal appearance
 const contactLink = document.querySelector("#contact-link")
 const contactModal = document.querySelector(".contact--modal")
+const modalCloseBtn = document.querySelector("#modal--close-btn")
 
 contactLink.addEventListener("click", () => {
     contactModal.style.display = "block"
+})
+
+modalCloseBtn.addEventListener("click", () => {
+    contactModal.style.display = "none"
 })
